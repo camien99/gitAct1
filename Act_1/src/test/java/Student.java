0cc5b3a127a861d972b77b583c8760b1687707f8
camie <camien99@gmail.com>
2021-04-04 10:43:38 +0800

    Testing first commit

---
+++ Act_1/src/test/java/Student.java
@@ -0,0 +1,29 @@
+
+class Student {
+
+	int studentNumber;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + studentNumber;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (studentNumber != other.studentNumber)
+			return false;
+		return true;
+	}
+	
+	
+}
