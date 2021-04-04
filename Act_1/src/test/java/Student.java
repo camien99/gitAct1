33a0e33c1db3f5c22f79a9674be145b5a8b7be9a
camie <camien99@gmail.com>
2021-04-04 11:01:40 +0800

    edits

--- Act_1/src/test/java/Student.java
+++ Act_1/src/test/java/Student.java
@@ -30,7 +30,7 @@ class Student {
 	@Override
 	
 	public int hashCode() {
-		final int prime =31;
+		final int prime = 31;
 		int result = 1;
 		result = prime * result + studentNumber;
 		return result;
