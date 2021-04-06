7245cbf29c9d4b18b0df2b4400a3392f42e8233d
licsiangelo <80447890+licsiangelo@users.noreply.github.com>
2021-04-06 16:34:39 +0800

    added sce

---
+++ Act_1/src/main/java/ScheduleConflictException.java
@@ -0,0 +1,10 @@
+
+public class ScheduleConflictException extends RuntimeException {
+
+
+	public ScheduleConflictException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
