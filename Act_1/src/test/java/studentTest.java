aad053b6442ffbc374d1b19162b5e1fea63d67bd
licsiangelo <80447890+licsiangelo@users.noreply.github.com>
2021-04-06 12:07:13 +0800

    Update studentTest.java

--- Act_1/src/test/java/studentTest.java
+++ Act_1/src/test/java/studentTest.java
@@ -6,12 +6,12 @@ class studentTest {
 
 	@Test
 	void enlist_2_sections_no_sched_overlap() {
-		//1 student 2 sections
+		//1 student 2 sections no overlap sched
 		Student student = new Student(1, Collections.emptyList());
 		Section sec1 = new Section("A", new Schedule(Days.MTH, Period.H0830));
 		Section sec2 = new Section("B", new Schedule(Days.TF, Period.H1000));
 		
-		//enlistment on both sections
+		//enlistment in both sections
 		student.enlist(sec1);
 		student.enlist(sec2);
 		
@@ -24,4 +24,17 @@ class studentTest {
 		
 	}
 
+	@Test
+	void enlist_2_sections_same_schedule() {
+		//1 student 2 sections w/ same sched
+		Student student = new Student(1, Collections.emptyList());
+		Section sec 1 = new Section("A", new Schedule(Days.MTH, Period.H0830));
+		Section sec 2 = new Section("B", new Schedule(Days.MTH, Period.H0830));
+		
+		//enlistment in both sections
+		student.englist(sec1);
+		
+		//exception thrown in the second enlistment
+		assertThrows(Exception.class, () -> student.enlist(sec2));
+			
 }
